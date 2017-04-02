8b02c34c45ab756d9ecba2ce772e43e1416d47ae
Mr Robot <t.a.r.i@icloud.com>
2017-04-02 15:09:01 -0400

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: django
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+include_recipe 'django::install_packages'
---
+++ recipes/install_packages.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: django
+# Recipe:: install_packages.rb
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'python-django'
